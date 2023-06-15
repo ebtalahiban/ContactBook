c2bd99fc1ab2e88d099c585f461e9f9c9b20d3ab
Erica Talahiban <ebtalahiban@up.edu.ph>
2023-06-16 01:19:07 +0800

    Implement a ContactBook constructor and initial methods.

--- ContactBook.java
+++ ContactBook.java
@@ -1,4 +1,26 @@
 public class ContactBook {
+
+    public ContactBook(){
+        int max = 100;
+        String[][] contactbook = new String[max][2];
+    }
+
+    public void addContact(){
+
+    }
+
+    public void searchContact(){
+
+    }
+
+    public void deleteContact(){
+
+    }
+
+    public void displayContact(){
+
+    }
+
     public static void main(String[] args) {
         ContactBook contactbook = new ContactBook();
     }
