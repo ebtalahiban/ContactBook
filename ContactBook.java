e4d2d3545fe9202ac03a4dec4542e00c07d116b1
Erica Talahiban <ebtalahiban@up.edu.ph>
2023-06-16 02:11:34 +0800

    Update searchContact method.

--- ContactBook.java
+++ ContactBook.java
@@ -1,6 +1,6 @@
 public class ContactBook {
 
-    private int maxContacts = 100;
+    private final static int maxContacts = 100;
     private String[][] contactbook;
     private int contactCount;
 
@@ -8,29 +8,41 @@ public class ContactBook {
         this.contactbook = new String[maxContacts][2];
         this.contactCount = 0;
     }
-
+    /** Add new contact to the contact book.*/
     public void addContact(String name, String phoneNumber){
         if(contactCount <= maxContacts){
             contactbook[contactCount][0] = name;
             contactbook[contactCount][1] = phoneNumber;
-            contactCount++;
             System.out.println("Contact successfully added.");
+            contactCount++;
         }
         else{
             System.out.println("Contact Book is full.");
         }
     }
-
-    public void searchContact(){
-
+    /**Search information in the contact book using name as key.*/
+    public void searchContact(String name){
+        for(int i=0; i<contactCount; i++){
+            if(contactbook[i][0].equals(name)){
+                System.out.println("Contact found.");
+                System.out.println("Name: " + contactbook[i][0]);
+                System.out.println("Phone number : " + contactbook[i][1]);
+            }
+            else{
+                System.out.println("Contact does not exist.");
+            }
+        }
     }
-
+    /**Delete an item in the contact book.*/
     public void deleteContact(){
 
     }
-
+    /**Display all the contact in the contact book.*/
     public void displayContact(){
-
+        for(int i=0; i<contactCount; i++){
+            System.out.println("Name: " + contactbook[i][0]);
+            System.out.println("Phone number : " + contactbook[i][1]);
+        }
     }
 
     public static void main(String[] args) {
